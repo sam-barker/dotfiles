77586830367bb3692cca1fca3b8d571615a35b45
Samuel Barker <dev@sam-barker.co.uk>
2019-08-20 10:50:22 +0100

    Checkpoint

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -1,48 +1,32 @@
 #!/usr/bin/env bash
-
-## APPS
-# VSCode
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
 alias c="code"
-
-## NAVIGATION
-alias l='ls'
-
-## CREATION
-alias mkdir="mkdir -p"
-
-## TERMINAL MANAGEMENT
 alias cl="clear"
+alias dc="docker-compose"
 alias ex="exit"
-
-## NPM
+alias l='ls'
+alias mkdir="mkdir -p"
 alias n="npm"
-alias nr="npm run"
 alias ncl="rm -rf node_modules && n i"
-
-## NVM
+alias nr="npm run"
 nvm() {
   source $(brew --prefix nvm)/nvm.sh
   nvm use default --silent
   nvm
 }
-
-## GIT
 alias g="git"
 alias ga="git add"
 alias gacm="git add . && git commit -m"
-alias gs="git status"
-alias gd="git diff"
-alias glog="git log --oneline --decorate --graph"
-alias gst="git stash"
+alias gcheck="git add . && git commit -m 'Checkpoint' && git rebase -i HEAD~2 && git push --force"
+alias gcl="git clone"
+alias gclean="git branch | grep -v 'master' | xargs git branch -D"
 alias gcm="git commit -m"
+alias gco="git checkout"
+alias gd="git diff"
+alias gnb="git checkout -b"
+alias glog="git log --oneline --decorate --graph"
 alias gpl="git pull"
 alias gpsh='[[ -z $(git config "branch.$(git symbolic-ref --short HEAD).merge") ]] && git push -u origin $(git symbolic-ref --short HEAD) || git push'
-alias gcl="git clone"
-alias gnb="git checkout -b"
-alias gco="git checkout"
-alias gclean="git branch | grep -v 'master' | xargs git branch -D"
-alias gcheck="git add . && git commit -m 'Checkpoint' && git rebase -i HEAD~2 && git push --force"
-
-## Docker
-alias dc="docker-compose"
+alias gs="git status"
+alias gst="git stash"
